f080a34d94401dff4ece2656b54392f60b3b01db
Arom Jhee <aromjhee@gmail.com>
2020-05-01 13:15:26 -0700

    Wrote all specs for saveItems()

--- test/save-items-spec.js
+++ test/save-items-spec.js
@@ -2,10 +2,16 @@ const { expect } = require('chai');
 const { saveItems } = require('../save-items');
 describe("The saveItems function", () => {
   it('adds the new item to the list', () => {
-    expect.fail('please write this test');
+    const items = [ {title: 'Title A', category: 'Category A'} ];
+    const newItem = {title: 'Title 1', category: 'Category 1'};
+    const result = saveItems(items, newItem)
+    expect(result).to.include(newItem);
   });
 
   it('makes sure the result and the original are different', () => {
-    expect.fail('please write this test');
+    const items = [{ title: 'Title A', category: 'Category A' }];
+    const newItem = { title: 'Title 1', category: 'Category 1' };
+    const result = saveItems(items, newItem)
+    expect(result).to.not.equal(items);
   });
 });
