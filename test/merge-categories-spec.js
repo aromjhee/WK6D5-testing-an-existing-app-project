c99134afaedf4a5f2a5b5cfc91c2860b1de7895f
Arom Jhee <aromjhee@gmail.com>
2020-05-01 10:24:42 -0700

    Wrote specs for mergeCategories()'s first context

--- test/merge-categories-spec.js
+++ test/merge-categories-spec.js
@@ -12,14 +12,42 @@ describe("mergeCategories()", () => {
     `;
 
     it("should return no <li>s for no categories", () => {
+      // arrange: set up values
+      const categories = [];
+      // act: single line of code for single action
+      const result = mergeCategories(template, categories, 'li');
+      //assert; assert as much as from that act
+      expect(result).to.include('<div>');
+      expect(result).to.include('</div>');
+      expect(result).to.include('<ul>');
+      expect(result).to.include('</ul>');
+      expect(result).to.not.include('<li>');
+      expect(result).to.not.include('</li>');
+      expect(result).to.not.include('<!-- Content here -->');
     });
 
     it("should return a single <li> for one category", () => {
-      expect.fail('please write this test');
+      const categories = ['Difficult'];
+      const result = mergeCategories(template, categories, 'li');
+      expect(result).to.include('<div>');
+      expect(result).to.include('</div>');
+      expect(result).to.include('<ul>');
+      expect(result).to.include('</ul>');
+      expect(result).to.include('<li>Difficult</li>');
+      expect(result).to.not.include('<!-- Content here -->');
     });
 
     it("should return an <li> for each category", () => {
-      expect.fail('please write this test');
+      const categories = ['Difficult', 'Music', 'Chill'];
+      const result = mergeCategories(template, categories, 'li');
+      expect(result).to.include('<div>');
+      expect(result).to.include('</div>');
+      expect(result).to.include('<ul>');
+      expect(result).to.include('</ul>');
+      expect(result).to.include('<li>Difficult</li>');
+      expect(result).to.include('<li>Music</li>');
+      expect(result).to.include('<li>Chill</li>');
+      expect(result).to.not.include('<!-- Content here -->');
     });
   });
 
